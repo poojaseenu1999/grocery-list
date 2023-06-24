8d0c39e9e671df33b5c488ed1cd6ff7d42cc2037
Pooja Srinivasan <poojaseenu@gmail.com>
2023-06-23 22:52:45 -0400

    html

---
+++ app.js
@@ -0,0 +1,11 @@
+let alert = document.querySelector('.alert');
+let form = document.querySelector('.grocery-form')
+let input = document.querySelector('.input');
+let submit = document.querySelector('.submit');
+let container = document.querySelector('.container');
+let list = document.querySelector('.grocery-container');
+let clear = document.querySelector('.clear');
+
+
+// declaring constans
+
